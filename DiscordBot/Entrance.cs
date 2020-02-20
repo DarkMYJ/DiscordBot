198d1d34f2b028df31804318c0d6a1d5c8e35ac6
DarkMYJ <DarkMYJ@github.com>
2020-02-20 09:35:13 +0700

    Add project files.

---
+++ DiscordBot/Entrance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordBot
+{
+    class Entrance
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
